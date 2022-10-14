c0b2458e90e8fd25e6cf2be929181a5a03929845
Harmandeep-Kaur <harmanchohan9@gmail.com>
2022-10-14 13:53:41 +0530

    form validation for required field

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,58 @@
 <template>
   <div class="q-pa-md">
-    <q-stepper v-model="step" ref="stepper" color="primary" animated>
+    <q-stepper v-model="step" dark ref="stepper" color="primary" animated>
       <q-step :name="1" title="Client Info" icon="info" :done="step > 1">
         <div class="row">
           <div class="col">
             <q-radio
+              dark
               v-model="formData.opportunity_client_type"
               val="Individual"
             >
               Individual</q-radio
             >
-            <q-radio v-model="formData.opportunity_client_type" val="Company">
+            <q-radio
+              dark
+              v-model="formData.opportunity_client_type"
+              val="Company"
+            >
               Company/Trust</q-radio
             >
           </div>
           <div class="col">
-            <q-radio v-model="formData.opportunity_client_no" val="1 Person">
+            <q-radio
+              dark=""
+              v-model="formData.opportunity_client_no"
+              val="1 Person"
+            >
               1 Person
             </q-radio>
-            <q-radio v-model="formData.opportunity_client_no" val="2 Person">
+            <q-radio
+              dark
+              v-model="formData.opportunity_client_no"
+              val="2 Person"
+            >
               2 Person
             </q-radio>
           </div>
         </div>
         <div class="row">
           <q-radio
+            dark
             v-model="formData.opportunity_martial_status"
             val="notMarried"
           >
             Not Married</q-radio
           >
           <q-radio
+            dark
             v-model="formData.opportunity_martial_status"
             val="married-anc"
           >
             Married-ANC</q-radio
           >
           <q-radio
+            dark
             v-model="formData.opportunity_martial_status"
             val="married-cop"
           >
@@ -46,82 +62,101 @@
         <div class="row">
           <div class="col">
             <q-input
+              ref="firstnameRef"
+              dark
               v-model="formData.opportunity_firstname"
               square
               outlined
               label="First Name*"
+              :rules="requiredRules"
             />
           </div>
           <div class="col">
             <q-input
+              ref="lastnameRef"
+              dark
               v-model="formData.opportunity_lastname"
               square
               outlined
               label="Last Name*"
+              :rules="requiredRules"
             />
           </div>
         </div>
         <div class="row">
           <div class="col">
             <q-input
+              ref="idRef"
+              dark
+              type="number"
               v-model="formData.opportunity_id"
               square
               outlined
               label="ID Number*"
+              :rules="requiredRules"
             />
           </div>
           <div class="col">
             <q-input
+              dark
+              ref="emailRef"
               v-model="formData.opportunity_email"
               type="email"
               square
               outlined
-              label="Email"
+              label="Email*"
+              :rules="requiredRules"
             />
           </div>
         </div>
         <div class="row">
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_mobile"
               square
               outlined
               type="tel"
               label="MobileNumber*"
             >
-              <template v-slot:prepend>
+              <!-- <template v-slot:prepend>
                 <q-select
+                  dark
                   borderless
                   style="width: 150px"
                   v-model="countryCode"
                   :options="countryCodeOptions"
                   label="Country Code"
                 />
-              </template>
+              </template> -->
             </q-input>
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_landline"
               square
               outlined
+              type="tel"
               label="Landline"
             >
-              <template v-slot:prepend>
+              <!-- <template v-slot:prepend>
                 <q-select
+                  dark
                   borderless
                   style="width: 150px"
                   v-model="countryCode"
                   :options="countryCodeOptions"
                   label="Country Code"
                 />
-              </template>
+              </template> -->
             </q-input>
           </div>
         </div>
         <div class="row">
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_postal_address"
               square
               type="textarea"
@@ -130,6 +165,7 @@
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_residental_address"
               square
               type="textarea"
@@ -140,6 +176,7 @@
       </q-step>
 
       <q-step
+        dark
         :name="2"
         title="Additional Details"
         icon="account_circle"
@@ -147,19 +184,19 @@
       >
         <div class="row">
           <div class="col">
-            <q-radio v-model="formData.opportunity_pay_type" val="Bonded">
+            <q-radio dark v-model="formData.opportunity_pay_type" val="Bonded">
               Bonded</q-radio
             >
-            <q-radio v-model="formData.opportunity_pay_type" val="Cash">
+            <q-radio dark v-model="formData.opportunity_pay_type" val="Cash">
               Cash</q-radio
             >
           </div>
           <div class="col">
             <span><b>Floor Plan</b></span>
-            <q-radio v-model="formData.opportunity_pat_category" val="A">
+            <q-radio dark v-model="formData.opportunity_pat_category" val="A">
               A
             </q-radio>
-            <q-radio v-model="formData.opportunity_pat_category" val="C">
+            <q-radio dark v-model="formData.opportunity_pat_category" val="C">
               C
             </q-radio>
           </div>
@@ -167,19 +204,24 @@
         <div class="row">
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_best_price"
               label="Best Price"
             />
           </div>
           <div class="col">
-            <q-input v-model="formData.opportunity_deposite" label="Deposit" />
+            <q-input
+              dark
+              v-model="formData.opportunity_deposite"
+              label="Deposit"
+            />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_deposite_date"
               mask="date"
               label="yy/mm/dd"
-              :rules="['date']"
             >
               <template v-slot:append>
                 <q-icon name="event" class="cursor-pointer">
@@ -202,27 +244,32 @@
                 </q-icon>
               </template>
             </q-input>
-            <!-- <q-input v-model="formData.opportunity_deposite_date" label="Deposit Date Paid" /> -->
           </div>
         </div>
         <div class="row">
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_parking_base"
               label="Parking Base"
             />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_parking_cost"
               label="Parking Cost"
             />
           </div>
           <div class="col">
-            <q-radio v-model="formData.opportunity_stove_option" val="standard">
+            <q-radio
+              dark
+              v-model="formData.opportunity_stove_option"
+              val="standard"
+            >
               standard
             </q-radio>
-            <q-radio v-model="formData.opportunity_stove_option" val="gas">
+            <q-radio dark v-model="formData.opportunity_stove_option" val="gas">
               Gas
             </q-radio>
           </div>
@@ -243,12 +290,14 @@
           <div class="col"></div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_bond_amount"
               label="Bond Amount Required"
             />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_contract_price"
               label="Contract Price"
             />
@@ -257,12 +306,14 @@
         <div class="row">
           <div class="col-4">
             <q-input
+              dark
               v-model="formData.opportunity_additional_cost"
               label="Additional Extra Cost"
             />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_notes"
               square
               autogrow
@@ -274,30 +325,33 @@
         <div class="row">
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_gardenNumber"
               label="Garden Number"
             />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_gardenSize"
               label="Garden Size"
             />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_originalBayNo"
               label="Original Bay Number"
             />
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_parkingBayNo"
               label="Parking Bay Number"
             />
           </div>
         </div>
-        <hr />
         <div
           class="row"
           style="text-align: center; font-size: 20px; margin-top: 40px"
@@ -312,7 +366,6 @@
             <b>Enclosed Balcony: 0</b>
           </div>
         </div>
-        <hr />
         <div
           class="row"
           style="text-align: center; font-size: 17px; margin-top: 40px"
@@ -322,10 +375,18 @@
               <b>Spare Room: </b>
             </div>
             <div style="margin-top: 15px">
-              <q-radio v-model="formData.opportunity_spareRoom" val="bedroom">
+              <q-radio
+                dark
+                v-model="formData.opportunity_spareRoom"
+                val="bedroom"
+              >
                 Bedroom</q-radio
               >
-              <q-radio v-model="formData.opportunity_spareRoom" val="study">
+              <q-radio
+                dark
+                v-model="formData.opportunity_spareRoom"
+                val="study"
+              >
                 Study</q-radio
               >
             </div>
@@ -335,10 +396,10 @@
               <b>Mood: </b>
             </div>
             <div style="margin-top: 15px">
-              <q-radio v-model="formData.opportunity_mood" val="allure">
+              <q-radio dark v-model="formData.opportunity_mood" val="allure">
                 Allure</q-radio
               >
-              <q-radio v-model="formData.opportunity_mood" val="serene">
+              <q-radio dark v-model="formData.opportunity_mood" val="serene">
                 Serene</q-radio
               >
             </div>
@@ -348,10 +409,14 @@
               <b>Flooring: </b>
             </div>
             <div style="margin-top: 15px">
-              <q-radio v-model="formData.opportunity_flooring" val="tiles">
+              <q-radio dark v-model="formData.opportunity_flooring" val="tiles">
                 Tiles</q-radio
               >
-              <q-radio v-model="formData.opportunity_flooring" val="laminate">
+              <q-radio
+                dark
+                v-model="formData.opportunity_flooring"
+                val="laminate"
+              >
                 Laminate</q-radio
               >
             </div>
@@ -366,9 +431,9 @@
         icon="upload"
         :done="step > 3"
       >
-        <!-- <q-form @input="fileUpload" class="q-gutter-md"> -->
         <q-form id="formotp">
           <q-file
+            dark
             filled
             v-model="formData.opportunity_otp"
             @input="fileUpload"
@@ -382,6 +447,7 @@
             </template>
           </q-file>
           <q-file
+            dark
             ref="uploadId"
             filled
             v-model="formData.opportunity_uploadId"
@@ -395,6 +461,7 @@
           </q-file>
           <q-file
             filled
+            dark
             v-model="formData.opportunity_addressproof"
             @input="fileUpload"
             label="Proof of Address"
@@ -406,6 +473,7 @@
           </q-file>
           <q-file
             filled
+            dark
             v-model="formData.opportunity_upload_deposite"
             @input="fileUpload"
             label="Upload deposit POP"
@@ -417,6 +485,7 @@
           </q-file>
           <q-file
             filled
+            dark
             v-model="formData.opportunity_upload_statement"
             @input="fileUpload"
             label="Upload Bank Statements"
@@ -428,6 +497,7 @@
           </q-file>
           <q-file
             filled
+            dark
             v-model="formData.opportunity_upload_lastThree"
             @input="fileUpload"
             label="Upload latest 3 months Payslips"
@@ -444,6 +514,7 @@
         <div class="row">
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_sale_agreement"
               square
               outlined
@@ -452,28 +523,30 @@
           </div>
           <div class="col">
             <q-input
+              dark
               v-model="formData.opportunity_sale_mobile"
               square
               outlined
               label="Mobile Number*"
             >
-              <template v-slot:prepend>
+              <!-- <template v-slot:prepend>
                 <q-select
                   style="width: 150px"
                   v-model="formData.countryCode"
                   :options="countryCodeOptions"
                   label="Country Code"
                 />
-              </template>
+              </template> -->
             </q-input>
           </div>
         </div>
       </q-step>
 
       <template v-slot:navigation>
+        <span>{{ error }}</span>
         <q-stepper-navigation>
           <q-btn
-            @click="submitForm(step)"
+            @click.prevent.stop="submitForm"
             color="primary"
             :label="step === 4 ? 'Submit' : 'Next'"
           />
@@ -493,41 +566,62 @@
 
 <script>
 import { ref } from "vue";
-import axios from "axios";
 import { useStore } from "vuex";
+import { useQuasar } from "quasar";
 
 export default {
   name: "App",
   setup() {
     const store = useStore();
     const step = ref(1);
+    const firstnameRef = ref(null);
+    const lastnameRef = ref(null);
+    const idRef = ref(null);
+    const emailRef = ref(null);
+
     const countryCode = null;
     const countryCodeOptions = ["+44", "+91", "+54"];
     const formData = store.state.formData;
-    return { step, countryCode, countryCodeOptions, formData };
+    const error = "";
+    const load = useQuasar();
+    return {
+      step,
+      firstnameRef,
+      lastnameRef,
+      idRef,
+      emailRef,
+      countryCode,
+      countryCodeOptions,
+      formData,
+      error,
+      load,
+      requiredRules: [(val) => (val && val.length > 0) || "Field Required"],
+    };
   },
   methods: {
-    fileUpload(e) {
-      const file = e.target?.files[0];
-
-      let formData = new FormData();
-      formData.append("file", file);
-      axios
-        .post("http://localhost:3008/uploadfile", formData)
-        .then((res) => {
-          if (res) {
-            console.log(res);
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
     async submitForm() {
       if (this.step === 4) {
         this.$store.dispatch("postData", this.formData);
+      } else if (this.step === 1) {
+        this.firstnameRef.validate();
+        this.lastnameRef.validate();
+        this.idRef.validate();
+        this.emailRef.validate();
+
+        if (
+          this.firstnameRef.hasError ||
+          this.lastnameRef.hasError ||
+          this.idRef.hasError ||
+          this.emailRef.hasError
+        ) {
+          // form has error
+          console.log("error");
+        } else {
+          this.$refs.stepper.next();
+        }
+      } else {
+        this.$refs.stepper.next();
       }
-      this.$refs.stepper.next();
     },
   },
 };
--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -2,9 +2,14 @@
 import './styles/quasar.sass'
 import '@quasar/extras/material-icons/material-icons.css'
 
+import {
+  Loading
+} from 'quasar'
+
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
+  config: { Loading },
   plugins: {
+    Loading
   }
 }
